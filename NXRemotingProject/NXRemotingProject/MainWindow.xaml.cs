6b542e1bb014b6558df60375bb11584dcbfbb69f
cgrooves <caleb.j.groves@gmail.com>
2018-03-28 13:53:42 -0600

    Solution built and setup

--- NXRemotingProject/NXRemotingProject/MainWindow.xaml.cs
+++ NXRemotingProject/NXRemotingProject/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace NXRemotingProject
 {
@@ -24,12 +26,68 @@ namespace NXRemotingProject
     {
         Session theSession;
         UFSession theUFSession;
+
+        string airFoilText; // complete text from the airfoil data
+        List<double[]> airFoilData = new List<double[]>(); // List of ordered pairs for [x,y] data on airfoil
         public MainWindow()
         {
             InitializeComponent();
 
-            theSession = (Session)Activator.GetObject(typeof(Session), "http://localhost:4567/NXOpenSession");
-            theUFSession = (UFSession)Activator.GetObject(typeof(UFSession), "http://localhost:4567/UFSession");
+            //theSession = (Session)Activator.GetObject(typeof(Session), "http://localhost:4567/NXOpenSession");
+            //theUFSession = (UFSession)Activator.GetObject(typeof(UFSession), "http://localhost:4567/UFSession");
+        }
+
+        private void selectAirfoil(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog airFoilDialog = new OpenFileDialog();
+            //airFoilDialog.Filter = "Airfoil data (*.dat)|*.dat|All files (*.*)|*.*";
+            if (airFoilDialog.ShowDialog() == true)
+                airFoilText = File.ReadAllText(airFoilDialog.FileName);
+        }
+
+        // Parses the airfoil data from the UIUC Applied Aerodynamics site.
+        private void parseAirfoilText()
+        {
+            // To add to airFoilData the form is: airFoilData.Add(new double[] {value1, value2})
+            string[] airFoilTextLines = airFoilText.Split('\n');
+            int noLines = airFoilTextLines.GetLength(0);
+
+            // For each line of text in the airFoilText
+            for (int i = 0; i < noLines; i++)
+            {
+                // Try parsing out two doubles
+                string[] words = airFoilTextLines[i].Split(' ');
+                try
+                {
+                    double x = 0;
+                    double y = 0;
+
+                    bool parseX = double.TryParse(words[0], out x);
+                    bool parseY = double.TryParse(words[1], out y);
+                    
+                    if (parseX && parseY)
+                    {
+                        airFoilData.Add(new double[] { x, y });
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Line " + i + " is not a valid airfoil data point: " + airFoilTextLines[i]);
+                    }                                        
+                }
+                catch
+                {
+                    System.Console.WriteLine("Line " + i + " is not a valid airfoil data point: " + airFoilTextLines[i]);
+                }
+                // If it fails, it's not airfoil data, discard it and move on
+                // If it succeeds, add the doubles to the airFoilData
+            }
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            parseAirfoilText();
         }
     }
 }
