36d6080ab9eaa098d9339fea51da1d688b3ae93d
cgrooves <caleb.j.groves@gmail.com>
2018-04-04 16:03:34 -0600

    Added the code for points and lines from the airfoil data.

--- NXRemotingProject/NXRemotingProject/MainWindow.xaml.cs
+++ NXRemotingProject/NXRemotingProject/MainWindow.xaml.cs
@@ -92,7 +92,7 @@ namespace NXRemotingProject
             fileNew1.ItemType = "";
             fileNew1.Specialization = "";
             fileNew1.SetCanCreateAltrep(false);
-            fileNew1.NewFileName = @"C:\Program Files\Siemens\NX 10.0\UGII\";
+            fileNew1.NewFileName = @"C:\Program Files\Siemens\NX 10.0\UGII\airfoil.prt";
             fileNew1.MasterFileName = "";
             fileNew1.MakeDisplayedPart = true;
             NXOpen.NXObject nXObject1;
@@ -123,8 +123,84 @@ namespace NXRemotingProject
             sketchAlongPathBuilder1.Destroy();
 
             // Create points from airfoil data
-            // Create lines from point to point
-            // Create an extrude from those points
+            // allocate the containers for all of the NXOpen.3dpoints and NXOpen.Lines
+            int pointCount = airfoilData.Length;
+            NXOpen.Point3d[] airfoilPoints = new NXOpen.Point3d[pointCount];
+            NXOpen.Line[] airfoilLines = new NXOpen.Line[pointCount - 1];
+
+            // create the first point
+            airfoilPoints[0] = new NXOpen.Point3d(airfoilData[0][0], airfoilData[0][1], 0);
+
+            // loop through each subsequent point in the airFoilData
+            for (int i = 1; i < pointCount-1; i++)
+            {
+                // create the next point
+                airfoilPoints[i] = new NXOpen.Point3d(airfoilData[i][0], airfoilData[i][1], 0);
+                // create a line to connect it to the previous point
+                airfoilLines[i - 1] = workPart.Curves.CreateLine(airfoilPoints[i - 1], airfoilPoints[i]);
+                // add the line to the sketch
+                sketch1.AddGeometry(airfoilLines[i - 1], Sketch.InferConstraintsOption.InferCoincidentConstraints);
+            }
+
+            // create the last point
+            airfoilPoints[pointCount - 1] = new NXOpen.Point3d(airfoilData[pointCount - 1][0], airfoilData[pointCount - 1][1], 0);
+            // connect the last point to the first point
+            airfoilLines[pointCount - 2] = workPart.Curves.CreateLine(airfoilPoints[pointCount - 2], airfoilPoints[pointCount - 1]);
+
+            // Create an extrude from the sketch
+            NXOpen.Features.Feature nullNXOpen_Features_Feature = null;
+
+            NXOpen.Features.ExtrudeBuilder extrudeBuilder1;
+            extrudeBuilder1 = workPart.Features.CreateExtrudeBuilder(nullNXOpen_Features_Feature);
+
+            NXOpen.Section section1;
+            section1 = workPart.Sections.CreateSection(0.0095, 0.01, 0.5);
+            extrudeBuilder1.Section = section1;
+            extrudeBuilder1.AllowSelfIntersectingSection(true);
+            extrudeBuilder1.DistanceTolerance = 0.00001;
+            extrudeBuilder1.BooleanOperation.Type = NXOpen.GeometricUtilities.BooleanOperation.BooleanType.Create;
+
+            NXOpen.Body[] targetBodies1 = new NXOpen.Body[1];
+            NXOpen.Body nullNXOpen_Body = null;
+            targetBodies1[0] = nullNXOpen_Body;
+            extrudeBuilder1.BooleanOperation.SetTargetBodies(targetBodies1);
+            extrudeBuilder1.Limits.StartExtend.Value.RightHandSide = "0";
+            extrudeBuilder1.Limits.EndExtend.Value.RightHandSide = "100";
+            extrudeBuilder1.Draft.FrontDraftAngle.RightHandSide = "2";
+            extrudeBuilder1.Draft.BackDraftAngle.RightHandSide = "2";
+            extrudeBuilder1.Offset.StartOffset.RightHandSide = "0";
+            extrudeBuilder1.Offset.EndOffset.RightHandSide = "5";
+
+            NXOpen.GeometricUtilities.SmartVolumeProfileBuilder smartVolumeProfileBuilder1;
+            smartVolumeProfileBuilder1 = extrudeBuilder1.SmartVolumeProfile;
+
+            smartVolumeProfileBuilder1.OpenProfileSmartVolumeOption = false;
+            smartVolumeProfileBuilder1.CloseProfileRule = NXOpen.GeometricUtilities.SmartVolumeProfileBuilder.CloseProfileRuleType.Fci;
+            section1.DistanceTolerance = 0.01;
+            section1.ChainingTolerance = 0.0095;
+            section1.SetAllowedEntityTypes(NXOpen.Section.AllowTypes.OnlyCurves);
+
+            NXOpen.Features.Feature[] features1 = new NXOpen.Features.Feature[1];
+            NXOpen.Features.SketchFeature sketchFeature1 = (NXOpen.Features.SketchFeature)workPart.Features.FindObject("SKETCH(1)");
+            features1[0] = sketchFeature1;
+            NXOpen.CurveFeatureRule curveFeatureRule1;
+            curveFeatureRule1 = workPart.ScRuleFactory.CreateRuleCurveFeature(features1);
+            section1.AllowSelfIntersection(true);
+            NXOpen.SelectionIntentRule[] rules1 = new NXOpen.SelectionIntentRule[1];
+            rules1[0] = curveFeatureRule1;
+            NXOpen.NXObject nullNXOpen_NXObject = null;
+            NXOpen.Point3d helpPoint1 = new NXOpen.Point3d(38.604206403219, 2.59588648435302, 0.0);
+            section1.AddToSection(rules1, airfoilLines[0], nullNXOpen_NXObject, nullNXOpen_NXObject, helpPoint1, NXOpen.Section.Mode.Create, false);
+            NXOpen.Direction direction1;
+            direction1 = workPart.Directions.CreateDirection(sketch1, NXOpen.Sense.Forward, NXOpen.SmartObject.UpdateOption.WithinModeling);
+            extrudeBuilder1.Direction = direction1;
+            extrudeBuilder1.ParentFeatureInternal = false;
+            feature1 = extrudeBuilder1.CommitFeature();
+            NXOpen.Expression expression2;
+            expression2 = extrudeBuilder1.Limits.StartExtend.Value;
+            NXOpen.Expression expression3 = extrudeBuilder1.Limits.EndExtend.Value;
+            extrudeBuilder1.Destroy();
+
         }
     }
 }
